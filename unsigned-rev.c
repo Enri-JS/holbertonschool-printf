5714202da8a8b42600c70043700eafddafa5cb11
Enri-DJS <danieldejesus9654@gmail.com>
2024-12-01 00:42:15 -0400

    unsigned-rev.c

--- unsigned-rev.c
+++ unsigned-rev.c
@@ -1,7 +1,10 @@
-#include “header.h”
+#include "header.h"
 /**
- * op_unsigned - prints unsigned integer
- * return: always 0
+ * op_unsigned - prints an unsigned integer.
+ * @u: va_list containing the unsigned integer to be printed.
+ *
+ * Return: The number of characters printed (excluding the null terminator).
+ * Always returns the number of digits printed.
  */
 int op_unsigned(va_list u)
 {
@@ -21,8 +24,11 @@ return (len);
 }
 
 /**
- * op_reverse - prints in reverse
- * return: always 0
+ * op_reverse - prints a string in reverse.
+ * @r: va_list containing the string to be printed in reverse.
+ *
+ * Return: The number of characters printed.
+ * Always returns the length of the string (not including the null terminator)
  */
 int op_reverse(va_list r)
 {
