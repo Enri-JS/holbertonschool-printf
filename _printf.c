cc740835bce4fc74292e2f89715f3ba377f58dba
Enri-DJS <danieldejesus9654@gmail.com>
2024-12-01 01:05:53 -0400

    _printf.c

--- _printf.c
+++ _printf.c
@@ -11,43 +11,41 @@
  * It excludes the null terminator.
  * If there is an invalid format specifier, returns -1.
  */
-int _printf(const char *format, …)
+int _printf(const char *format, ...)
 {
 	va_list call;
 	unsigned int i, len = 0;
 
-
 	va_start(call, format);
 
-
-	if (!format || (format[0] == ‘%’ &&format[1] == ‘\0’))
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	for (i = 0; format[i] != ‘\0’; i++)
+
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == ‘%’)
+		if (format[i] == '%')
 		{
-			if (format[i + 1] == ‘%’)
+			if (format[i + 1] == '%')
 			{
-				_putchar(‘%’);
-				i = i + 1;
+				_putchar('%');
+				i++;
 				len++;
 			}
-			else if (op_select(format, i + 1) != ‘\0’)
+			else if (op_select(format, i + 1) != NULL)
 			{
-				length += op_select(format, i + 1)(call);
-				i = i + 1;
-
+				len += op_select(format, i + 1)(call);
+				i++;
 			}
 			else
 			{
 				_putchar(format[i]);
 				len++;
 			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			len++;
+			else
+			{
+				_putchar(format[i]);
+				len++;
+			}
 		}
 	}
 	va_end(call);
